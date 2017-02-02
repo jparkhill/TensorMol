69553d51898b9cf7d4664c507f069c8ee3e2dba4
jeherr <johnherr@gmail.com>
2017-02-02 09:58:56 -0500

    intendation

--- TensorMol/TFManage.py
+++ TensorMol/TFManage.py
@@ -7,138 +7,138 @@ import numpy as np
 import gc
 
 class TFManage:
-    """
-    	A manager of tensorflow instances which perform atom-wise predictions
-    	and parent of the molecular instance mangager.
-    """
-    def __init__(self, Name_="", TData_=None, Train_=True, NetType_="fc_sqdiff", RandomTData_=True, ntrain_=2000):
-    	"""
-    		Args:
-    			Name_: If not blank, will try to load a network with that name using Prepare()
-    			TData_: A TensorData instance to provide and process data.
-    			Train_: Whether to train the instances raised.
-    			NetType_: Choices of Various network architectures.
-    			RandomTData_: Modifes the preparation of training batches.
-    			ntrain_: Number of steps to train an element.
-    	"""
-    	self.path = "./networks/"
-    	if (Name_ != ""):
-    		# This will unpickle and instantiate TData...
-    		self.name = Name_
-    		self.Prepare()
-    		return
-    	self.TData = TData_
-    	if (RandomTData_==False):
-    		self.TData.Randomize=False
-    	self.NetType = NetType_
-    	self.n_train = ntrain_
-    	# All done if you're doing molecular calculations
-    	print self.TData.AvailableElements
-    	print self.TData.AvailableDataFiles
-    	print self.TData.SamplesPerElement
-    	self.name = self.TData.name+"_"+self.TData.dig.name+"_"+self.NetType
-    	print "--- TF will be fed by ---",self.TData.name
-    	self.TrainedAtoms=[] # In order of the elements in TData
-    	self.TrainedNetworks=[] # In order of the elements in TData
-    	self.Instances=[None for i in range(MAX_ATOMIC_NUMBER)] # In order of the elements in TData
-    	if (Train_):
-    		self.Train()
-    		return
-    	return
+	"""
+		A manager of tensorflow instances which perform atom-wise predictions
+		and parent of the molecular instance mangager.
+	"""
+	def __init__(self, Name_="", TData_=None, Train_=True, NetType_="fc_sqdiff", RandomTData_=True, ntrain_=2000):
+		"""
+			Args:
+				Name_: If not blank, will try to load a network with that name using Prepare()
+				TData_: A TensorData instance to provide and process data.
+				Train_: Whether to train the instances raised.
+				NetType_: Choices of Various network architectures.
+				RandomTData_: Modifes the preparation of training batches.
+				ntrain_: Number of steps to train an element.
+		"""
+		self.path = "./networks/"
+		if (Name_ != ""):
+			# This will unpickle and instantiate TData...
+			self.name = Name_
+			self.Prepare()
+			return
+		self.TData = TData_
+		if (RandomTData_==False):
+			self.TData.Randomize=False
+		self.NetType = NetType_
+		self.n_train = ntrain_
+		# All done if you're doing molecular calculations
+		print self.TData.AvailableElements
+		print self.TData.AvailableDataFiles
+		print self.TData.SamplesPerElement
+		self.name = self.TData.name+"_"+self.TData.dig.name+"_"+self.NetType
+		print "--- TF will be fed by ---",self.TData.name
+		self.TrainedAtoms=[] # In order of the elements in TData
+		self.TrainedNetworks=[] # In order of the elements in TData
+		self.Instances=[None for i in range(MAX_ATOMIC_NUMBER)] # In order of the elements in TData
+		if (Train_):
+			self.Train()
+			return
+		return
 
-    def Print(self):
-    	print "-- TensorMol, Tensorflow Manager Status--"
-    	return
+	def Print(self):
+		print "-- TensorMol, Tensorflow Manager Status--"
+		return
 
-    def Train(self):
-    	print "Will train a NNetwork for each element in: ", self.TData.name
-    	for i in range(len(self.TData.AvailableElements)):
-    		self.TrainElement(self.TData.AvailableElements[i])
-    	self.Save()
-    	return
+	def Train(self):
+		print "Will train a NNetwork for each element in: ", self.TData.name
+		for i in range(len(self.TData.AvailableElements)):
+			self.TrainElement(self.TData.AvailableElements[i])
+		self.Save()
+		return
 
-    def Save(self):
-    	print "Saving TFManager:",self.path+self.name+".tfm"
-    	self.TData.CleanScratch()
-    	f=open(self.path+self.name+".tfm","wb")
-    	pickle.dump(self.__dict__, f, protocol=1)
-    	f.close()
-    	return
+	def Save(self):
+		print "Saving TFManager:",self.path+self.name+".tfm"
+		self.TData.CleanScratch()
+		f=open(self.path+self.name+".tfm","wb")
+		pickle.dump(self.__dict__, f, protocol=1)
+		f.close()
+		return
 
-    def Load(self):
-    	print "Unpickling TFManager..."
-    	f = open(self.path+self.name+".tfm","rb")
-    	tmp=pickle.load(f)
-    	self.__dict__.update(tmp)
-    	f.close()
-    	print "TFManager Metadata Loaded, Reviving Networks."
-    	self.Print()
-    	return
+	def Load(self):
+		print "Unpickling TFManager..."
+		f = open(self.path+self.name+".tfm","rb")
+		tmp=pickle.load(f)
+		self.__dict__.update(tmp)
+		f.close()
+		print "TFManager Metadata Loaded, Reviving Networks."
+		self.Print()
+		return
 
-    def TrainElement(self, ele):
-    	print "Training Element:", ele
-    	if (self.TData.dig.eshape==None):
-    		raise Exception("Must Have Digester")
-    	# It's up the TensorData to provide the batches and input output shapes.
-    	if (self.NetType == "fc_classify"):
-    		self.Instances[ele] = Instance_fc_classify(self.TData, ele, None)
-    	elif (self.NetType == "fc_sqdiff"):
-    		self.Instances[ele] = Instance_fc_sqdiff(self.TData, ele, None)
-    	elif (self.NetType == "3conv_sqdiff"):
-    		self.Instances[ele] = Instance_3dconv_sqdiff(self.TData, ele, None)
-    	elif (self.NetType == "KRR_sqdiff"):
-    		self.Instances[ele] = Instance_KRR(self.TData, ele, None)
-    	else:
-    		raise Exception("Unknown Network Type!")
-    	#self.Instances[ele].train_prepare()
-    	#for step in range (0, 10):
-    		#self.Instances[ele].train_step(step)
-    	#tself.Instances[ele].test(step)
-    	self.Instances[ele].train(self.n_train) # Just for the sake of debugging.
-    	nm = self.Instances[ele].name
-    	# Here we should print some summary of the pupil's progress as well, maybe.
-    	if self.TrainedNetworks.count(nm)==0:
-    		self.TrainedNetworks.append(nm)
-    	if self.TrainedAtoms.count(ele)==0:
-    		self.TrainedAtoms.append(ele)
-    	self.Save()
-    	gc.collect()
-    	return
+	def TrainElement(self, ele):
+		print "Training Element:", ele
+		if (self.TData.dig.eshape==None):
+			raise Exception("Must Have Digester")
+		# It's up the TensorData to provide the batches and input output shapes.
+		if (self.NetType == "fc_classify"):
+			self.Instances[ele] = Instance_fc_classify(self.TData, ele, None)
+		elif (self.NetType == "fc_sqdiff"):
+			self.Instances[ele] = Instance_fc_sqdiff(self.TData, ele, None)
+		elif (self.NetType == "3conv_sqdiff"):
+			self.Instances[ele] = Instance_3dconv_sqdiff(self.TData, ele, None)
+		elif (self.NetType == "KRR_sqdiff"):
+			self.Instances[ele] = Instance_KRR(self.TData, ele, None)
+		else:
+			raise Exception("Unknown Network Type!")
+		#self.Instances[ele].train_prepare()
+		#for step in range (0, 10):
+			#self.Instances[ele].train_step(step)
+		#tself.Instances[ele].test(step)
+		self.Instances[ele].train(self.n_train) # Just for the sake of debugging.
+		nm = self.Instances[ele].name
+		# Here we should print some summary of the pupil's progress as well, maybe.
+		if self.TrainedNetworks.count(nm)==0:
+			self.TrainedNetworks.append(nm)
+		if self.TrainedAtoms.count(ele)==0:
+			self.TrainedAtoms.append(ele)
+		self.Save()
+		gc.collect()
+		return
 
-    def EvalElement(self, ele, test_input):
-    	return self.Instances[ele].evaluate(test_input)    # for debugging
+	def EvalElement(self, ele, test_input):
+		return self.Instances[ele].evaluate(test_input)    # for debugging
 
-    def Prepare(self):
-    	self.Load()
-    	self.Instances=[None for i in range(MAX_ATOMIC_NUMBER)] # In order of the elements in TData
-    	for i  in range (0, len(self.TrainedAtoms)):
-    		if (self.NetType == "fc_classify"):
-    			self.Instances[self.TrainedAtoms[i]] = Instance_fc_classify(None, self.TrainedAtoms[i], self.TrainedNetworks[i])
-    		elif (self.NetType == "fc_sqdiff"):
-    			self.Instances[self.TrainedAtoms[i]] = Instance_fc_sqdiff(None, self.TrainedAtoms[i], self.TrainedNetworks[i])
-    		elif (self.NetType == "3conv_sqdiff"):
-    			self.Instances[self.TrainedAtoms[i]] = Instance_3dconv_sqdiff(None, self.TrainedAtoms[i], self.TrainedNetworks[i])
-    		else:
-    			raise Exception("Unknown Network Type!")
-    	# Raise TF instances for each atom which have already been trained.
-    	return
+	def Prepare(self):
+		self.Load()
+		self.Instances=[None for i in range(MAX_ATOMIC_NUMBER)] # In order of the elements in TData
+		for i  in range (0, len(self.TrainedAtoms)):
+			if (self.NetType == "fc_classify"):
+				self.Instances[self.TrainedAtoms[i]] = Instance_fc_classify(None, self.TrainedAtoms[i], self.TrainedNetworks[i])
+			elif (self.NetType == "fc_sqdiff"):
+				self.Instances[self.TrainedAtoms[i]] = Instance_fc_sqdiff(None, self.TrainedAtoms[i], self.TrainedNetworks[i])
+			elif (self.NetType == "3conv_sqdiff"):
+				self.Instances[self.TrainedAtoms[i]] = Instance_3dconv_sqdiff(None, self.TrainedAtoms[i], self.TrainedNetworks[i])
+			else:
+				raise Exception("Unknown Network Type!")
+		# Raise TF instances for each atom which have already been trained.
+		return
 
-    def SampleAtomGrid(self, mol, atom, maxstep, ngrid):
-        # use TF instances for each atom.
-        return  self.TData.dig.UniformDigest(mol,atom,maxstep,ngrid)
+	def SampleAtomGrid(self, mol, atom, maxstep, ngrid):
+	    # use TF instances for each atom.
+	    return  self.TData.dig.UniformDigest(mol,atom,maxstep,ngrid)
 
-    def SmoothPOneAtom(self, mol_, atom_):
-        '''
-        	Uses smooth fitting of Go probability to pick the next point as predicted by the network
-        	This should eventually be made faster by doing all atoms of an element type at once.
-        '''
-        inputs = self.TData.dig.Emb(mol_, atom_, [mol_.coords[atom_]],False)
-        output = self.Instances[mol_.atoms[atom_]].evaluate(inputs)[0,0]
-        if(not np.all(np.isfinite(output))):
-        	print output
-        	raise Exception("BadTFOutput")
-        p = mol_.UseGoProb(atom_, output)
-        return p
+	def SmoothPOneAtom(self, mol_, atom_):
+		'''
+			Uses smooth fitting of Go probability to pick the next point as predicted by the network
+			This should eventually be made faster by doing all atoms of an element type at once.
+		'''
+		inputs = self.TData.dig.Emb(mol_, atom_, [mol_.coords[atom_]],False)
+		output = self.Instances[mol_.atoms[atom_]].evaluate(inputs)[0,0]
+		if(not np.all(np.isfinite(output))):
+			print output
+			raise Exception("BadTFOutput")
+		p = mol_.UseGoProb(atom_, output)
+		return p
 
 	def EvalRotAvForce(self, mol, RotAv=10):
 		"""
@@ -231,7 +231,7 @@ class TFManage:
 			p.fill(1.0)
 		for i in satoms:
 			if (i == atom ):
-#			if (i == atom or mol.atoms[i] == 1):  # we ignore the hyrodgen here
+		#			if (i == atom or mol.atoms[i] == 1):  # we ignore the hyrodgen here
 				continue
 			ele = mol.atoms[i]
 			# The conditional digest moves the coordinates of catom to all points in the grid, and then evaluates
@@ -244,7 +244,7 @@ class TFManage:
 				tmp_p = tmp_p/(np.sum(tmp_p**2))**0.5  #just normlized it...
 			else:
 				tmp_p.fill(1.0)
-#			tmp_p = np.log10(p)  # take the log..
+		#			tmp_p = np.log10(p)  # take the log..
 			p *=  tmp_p  # small p is what we want
 		p = np.absolute(p)
 		return xyz, p
@@ -278,9 +278,9 @@ class TFManage:
 			tmp_mol = Mol(mol.atoms[0:i+1], mol.coords[0:i+1])
 			inputs = (self.TData.dig.Emb(i, tmp_mol.atoms, tmp_mol.coords,  ((tmp_mol.coords)[i]).reshape((1,3))))[0]
 			inputs = np.array(inputs)
-                        inputs = inputs.reshape((1,-1))
+	                    inputs = inputs.reshape((1,-1))
 		        p = float(self.Instances[tmp_mol.atoms[i]].evaluate(inputs))
 			print ("i:",i, "p:", p, "logp:", math.log10(p))
-                        logP += math.log10(p)
+	                    logP += math.log10(p)
 		print ("logP:", logP)
 		return logP
