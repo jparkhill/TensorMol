c4f955d16215f450f0d009be5dfcb846f78f4d77
jeherr <johnherr@gmail.com>
2017-02-08 17:12:04 -0500

    switching LMAX back to 6

--- TensorMol/TMParams.py
+++ TensorMol/TMParams.py
@@ -15,7 +15,7 @@ class TMParams(dict):
 		# Parameters of MolEmb
 		self["RBFS"] = np.array([[0.1, 0.156787], [0.3, 0.3], [0.5, 0.5], [0.7, 0.7], [1.3, 1.3], [2.2,
 			2.4], [4.4, 2.4], [6.6, 2.4], [8.8, 2.4], [11., 2.4], [13.2,2.4], [15.4, 2.4]])
-		self["SH_LMAX"]=3
+		self["SH_LMAX"]=6
 		self["SH_NRAD"]=10
 		self["SH_ORTH"]=0
 		# SET GENERATION parameters
