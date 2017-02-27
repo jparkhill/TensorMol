1568d8eaf7ce554e5c46db0df43087e15842ec5d
John E. Herr <johnherr@gmail.com>
2017-02-27 16:43:06 -0500

    added temp opt RBFS

--- TensorMol/TMParams.py
+++ TensorMol/TMParams.py
@@ -14,8 +14,10 @@ class TMParams(dict):
 		self["GIT_REVISION"] = os.popen("git rev-parse --short HEAD").read()
 		self["check_level"] = 1 # whether to test the consistency of several things...
 		# Parameters of MolEmb
-		self["RBFS"] = np.array([[0.1, 0.156787], [0.3, 0.3], [0.5, 0.5], [0.7, 0.7], [1.3, 1.3], [2.2,
-			2.4], [4.4, 2.4], [6.6, 2.4], [8.8, 2.4], [11., 2.4], [13.2,2.4], [15.4, 2.4]])
+		# self["RBFS"] = np.array([[0.1, 0.156787], [0.3, 0.3], [0.5, 0.5], [0.7, 0.7], [1.3, 1.3], [2.2,
+		# 	2.4], [4.4, 2.4], [6.6, 2.4], [8.8, 2.4], [11., 2.4], [13.2,2.4], [15.4, 2.4]])
+		self["RBFS"] = np.array([[0.99438638, 0.41676905], [0.59320762, 0.26405542], [0.44094295, 0.4394062],
+  								[0.61691555, 0.61625545], [1.20242598, 2.17935545]])
 		self["SRBF"] = np.zeros((self["RBFS"].shape[0],self["RBFS"].shape[0]))
 		self["ORBFS"] = np.zeros((self["RBFS"].shape[0],self["RBFS"].shape[0]))
 		self["SH_LMAX"]=2
