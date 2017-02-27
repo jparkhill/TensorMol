1ca944511df04eda5a2ef9266e97dbbc7c90b686
John E. Herr <johnherr@gmail.com>
2017-02-27 07:38:03 -0500

    fix

--- TensorMol/TMParams.py
+++ TensorMol/TMParams.py
@@ -17,6 +17,7 @@ class TMParams(dict):
 		self["RBFS"] = np.array([[0.1, 0.156787], [0.3, 0.3], [0.5, 0.5], [0.7, 0.7], [1.3, 1.3], [2.2,
 			2.4], [4.4, 2.4], [6.6, 2.4], [8.8, 2.4], [11., 2.4], [13.2,2.4], [15.4, 2.4]])
 		self["SRBF"] = np.zeros((self["RBFS"].shape[0],self["RBFS"].shape[0]))
+		self["ORBFS"] = np.zeros((self["RBFS"].shape[0],self["RBFS"].shape[0]))
 		self["SH_LMAX"]=2
 		self["SH_NRAD"]=7
 		self["SH_ORTH"]=1
