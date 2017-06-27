cc77a4fed435042247c3b09892f997155c53b2c4
John E. Herr <johnherr@gmail.com>
2017-06-27 18:15:09 -0400

    queue runner stuff

--- TensorMol/TensorData.py
+++ TensorMol/TensorData.py
@@ -635,8 +635,8 @@ class TensorData_TFRecords(TensorData):
 	def _bytes_feature(self, value):
 		return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-	def _floats_feature(self, value):
-		return tf.train.Feature(floats_list=tf.train.FloatsList(value=[value]))
+	def _float_feature(self, value):
+		return tf.train.Feature(float_list=tf.train.FloatList(value=value))
 
 	def ConvertToTFRec(self, inputs, labels, name):
 		"""Converts a dataset to tfrecords."""
@@ -650,9 +650,8 @@ class TensorData_TFRecords(TensorData):
 			input_raw = inputs[index].tostring()
 			label_raw = labels[index].tostring()
 			example = tf.train.Example(features=tf.train.Features(feature={
-				'input_raw': self._bytes_feature(image_raw),
-				'label': self._bytes_feature(label_raw),}))
-
+				'input_raw': self._bytes_feature(input_raw),
+				'label_raw': self._bytes_feature(label_raw),}))
 			writer.write(example.SerializeToString())
 		writer.close()
 
@@ -683,8 +682,8 @@ class TensorData_TFRecords(TensorData):
 			for m in self.set.mols:
 				nofe[element] = nofe[element]+m.NumOfAtomsE(element)
 		truncto = [nofe[i] for i in range(MAX_ATOMIC_NUMBER)]
-		cases_list = [np.zeros(shape=tuple([nofe[element]*self.dig.NTrainSamples]+list(self.dig.eshape)), dtype=np.float64) for element in atypes]
-		labels_list = [np.zeros(shape=tuple([nofe[element]*self.dig.NTrainSamples]+list(self.dig.lshape)), dtype=np.float64) for element in atypes]
+		cases_list = [np.zeros(shape=tuple([nofe[element]*self.dig.NTrainSamples]+list(self.dig.eshape)), dtype=np.float32) for element in atypes]
+		labels_list = [np.zeros(shape=tuple([nofe[element]*self.dig.NTrainSamples]+list(self.dig.lshape)), dtype=np.float32) for element in atypes]
 		casep_list = [0 for element in atypes]
 		t0 = time.time()
 		ord = len(self.set.mols)
@@ -708,70 +707,13 @@ class TensorData_TFRecords(TensorData):
 				print "Likely you need to re-install MolEmb.", Ex
 		for element in atypes:
 			# Write the tfrecords file for this element.
+			t1=time.time()
 			ai = atypes.tolist().index(element)
 			data_name = self.path+name_+"_"+self.dig.name+"_"+str(element)+".tfrecords"
 			self.ConvertToTFRec(cases_list[ai][:casep_list[ai]], labels_list[ai][:casep_list[ai]], data_name)
 			self.AvailableDataFiles.append([data_name])
 			self.AvailableElements.append(element)
 			self.SamplesPerElement.append(casep_list[ai]*self.dig.NTrainSamples)
+			print "Time to save:", time.time()-t1
 		self.Save() #write a convenience pickle.
 		return
-
-	def read_and_decode(self, filename_queue):
-		reader = tf.TFRecordReader()
-		_, serialized_example = reader.read(filename_queue)
-		features = tf.parse_single_example(
-			serialized_example,
-			# Defaults are not specified since both keys are required.
-			features={
-				'input_raw': tf.FixedLenFeature([], tf.string),
-				'label': tf.FixedLenFeature([], tf.string),
-			})
-
-		# Convert from a string to the desired datatype
-		inputs = tf.decode_raw(features['input_raw'], tf.float32)
-		label = tf.cast(features['label'], tf.float32)
-		# OPTIONAL: Could reshape into a 28x28 image and apply distortions
-		# here.  Since we are not applying any distortions in this
-		# example, and the next step expects the image to be flattened
-		# into a vector, we don't bother.
-		return inputs, label
-
-
-	def inputs(train, batch_size, num_epochs):
-		"""Reads input data num_epochs times.
-		Args:
-			train: Selects between the training (True) and validation (False) data.
-			batch_size: Number of examples per returned batch.
-			num_epochs: Number of times to read the input data, or 0/None to train forever.
-		Returns:
-			A tuple (images, labels), where:
-				* images is a float tensor with shape [batch_size, mnist.IMAGE_PIXELS]
-					in the range [-0.5, 0.5].
-				* labels is an int32 tensor with shape [batch_size] with the true label,
-					a number in the range [0, mnist.NUM_CLASSES).
-		Note that an tf.train.QueueRunner is added to the graph, which
-		must be run using e.g. tf.train.start_queue_runners().
-		"""
-		if not num_epochs: num_epochs = None
-		filename = os.path.join(FLAGS.train_dir,
-					TRAIN_FILE if train else VALIDATION_FILE)
-
-		with tf.name_scope('input'):
-		filename_queue = tf.train.string_input_producer(
-						[filename], num_epochs=num_epochs)
-
-		# Even when reading in multiple threads, share the filename
-		# queue.
-		image, label = read_and_decode(filename_queue)
-
-		# Shuffle the examples and collect them into batch_size batches.
-		# (Internally uses a RandomShuffleQueue.)
-		# We run this in two threads to avoid being a bottleneck.
-		images, sparse_labels = tf.train.shuffle_batch(
-			[image, label], batch_size=batch_size, num_threads=2,
-			capacity=1000 + 3 * batch_size,
-			# Ensures a minimum amount of shuffling of examples.
-			min_after_dequeue=1000)
-
-		return images, sparse_labels
--- test_jeherr.py
+++ test_jeherr.py
@@ -24,8 +24,8 @@ S_Rad = MolEmb.Overlap_RBF(PARAMS)
 S_RadOrth = MatrixPower(S_Rad,-1./2)
 PARAMS["SRBF"] = S_RadOrth
 PARAMS["RandomizeData"] = True
-PARAMS["InNormRoutine"] = "MeanStd"
-PARAMS["OutNormRoutine"] = "MeanStd"
+# PARAMS["InNormRoutine"] = "MeanStd"
+# PARAMS["OutNormRoutine"] = "MeanStd"
 PARAMS["TestRatio"] = 0.2
 PARAMS["max_steps"] = 5000
 PARAMS["batch_size"] = 8000
@@ -460,18 +460,35 @@ def Brute_LJParams():
 	# print ins.LJFrc(p)
 
 def QueueReadertmp():
-	a=MSet("SmallMols")
+	a=MSet("ammonia_5rot")
 	a.Load()
 	TreatedAtoms = a.AtomTypes()
 	d = Digester(TreatedAtoms, name_="GauSH", OType_="Force")
 	tset = TensorData_TFRecords(a,d)
 	tset.BuildTrainMolwise("SmallMols",TreatedAtoms)
 
+def QueueTrainForces(set_ = "SmallMols", dig_ = "GauSH", BuildTrain_=True, numrot_=None):
+	if (BuildTrain_):
+		a=MSet(set_)
+		a.Load()
+		if numrot_ != None:
+			a = a.RotatedClone(numrot_)
+			a.Save(a.name+"_"+str(numrot_)+"rot")
+		TreatedAtoms = a.AtomTypes()
+		print "Number of Mols: ", len(a.mols)
+		d = Digester(TreatedAtoms, name_=dig_, OType_="Force")
+		tset = TensorData_TFRecords(a,d)
+		tset.BuildTrainMolwise(set_,TreatedAtoms)
+	else:
+		tset = TensorData(None,None,set_+"_"+dig_)
+	# manager=TFManage("",tset,False,"fc_sqdiff_queue")
+	# manager.TrainElement(1)
+
 def TestForces():
-	#a=MSet("chemspid")
-	a=MSet("SmallMols")
+	a=MSet("chemspid")
+	# a=MSet("SmallMols")
 	a.Load()
-	manager=TFManage("SmallMols_20rot_GauSH_fc_sqdiff", None, False)
+	manager=TFManage("SmallMols_GauSH_fc_sqdiff", None, False)
 	err = np.zeros((32000,3))
 	ntest = 0
 	for mol in a.mols:
@@ -493,15 +510,15 @@ def TestForces():
 
 
 # InterpoleGeometries()
-# ReadSmallMols(set_="chemspid", dir_="/media/sdb2/jeherr/TensorMol/datasets/chemspider_data/*/", energy=True, forces=True)
+# ReadSmallMols(set_="chemspid1", dir_="/media/sdb2/jeherr/TensorMol/datasets/chemspider1_data/*/", forces=True)
 # TrainKRR(set_="SmallMols_rand", dig_ = "GauSH", OType_="Force")
 # RandomSmallSet("SmallMols", 30000)
 # BasisOpt_KRR("KRR", "SmallMols_rand", "GauSH", OType = "Force", Elements_ = [1,6,7,8])
 # BasisOpt_Ipecac("KRR", "ammonia_rand", "GauSH")
-TestIpecac()
+# TestIpecac()
 # TestBP()
 # TestANI1()
-# TrainForces(set_ = "SmallMols", BuildTrain_=False, numrot_=None)
+# TrainForces(set_ = "ammonia_5rot", BuildTrain_=True, numrot_=None)
 # OptTFForces(set_ = "peptide", mol=0)
 # TestOCSDB()
 # TestNeb()
@@ -511,6 +528,7 @@ TestIpecac()
 # TestMorphIR()
 # Brute_LJParams()
 # QueueReadertmp()
+QueueTrainForces(set_ = "SmallMols", BuildTrain_=True, numrot_=1)
 # TestForces()
 
 
