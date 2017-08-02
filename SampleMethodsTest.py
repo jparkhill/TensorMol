c952b9035b59b08aedcdb63198067d4a2a449d67
David Toth <dtoth1@nd.edu>
2017-08-02 15:16:16 -0400

    Sampling methods updates

--- SampleMethodsTest.py
+++ SampleMethodsTest.py
@@ -8,34 +8,43 @@ from TensorMol import *
 import os
 import numpy as np
 from math import *
+from TensorMol.ElectrostaticsTF import *
 
-def GetEnergyAndForceFromManager(MName_):
+def GetEnergyAndForceFromManager(MName_, set_):
+	"""
+	MName: name of the manager. (specifies sampling method.)
+	"""
+	# If you wanna do eq.
 	a = MSet("sampling_mols")
 	a.ReadXYZ()
+	#
+	a = MSet(set_)
+	a.Load()
 	TreatedAtoms = a.AtomTypes()
 	d = MolDigester(TreatedAtoms, name_="ANI1_Sym_Direct", OType_="AtomizationEnergy")
 	tset = TensorMolData_BP_Direct_Linear(a, d, order_=1, num_indis_=1, type_="mol",  WithGrad_ = True)
-	# PARAMS["hidden1"] = 200
-	# PARAMS["hidden2"] = 200
-	# PARAMS["hidden3"] = 200
 	PARAMS["tf_prec"] = "tf.float64"
 	PARAMS["GradScalar"] = 1
 	PARAMS["NeuronType"] = "relu"
 	PARAMS["HiddenLayers"] = [512,512,512]
 	manager = TFMolManage(MName_ , tset, False, RandomTData_=False, Trainable_=False)
-	energies=[]
-	gradients=[]
-	for mol in a.mols:
-		# mol.Distort(0.01)
+	EnergyError=[]
+	ForceError=[]
+	for mol in b.mols:
+		mol.properties["atomization"] = mol.properties["energy"]
+		for i in range(0, mol.NAtoms()):
+			mol.properties["atomization"] -= ele_E_david[mol.atoms[i]]
+		q_en = mol.properties["atomization"]
+		#qchem_energies.append(q_en)
 		en,grad=manager.Eval_BPEnergy_Direct_Grad_Linear(Mol(mol.atoms, mol.coords))
-		energies.append(en)
-		gradients.append(grad/-JOULEPERHARTREE)
-	print energies
-	print gradients
-	return energies,gradients
+		#tm_energies.append(en)
+		#tm_gradients.append(grad/-JOULEPERHARTREE)
+		# Subtract square, sqrt. add to EnergyError List.
+	# rms_energy = np.sqrt(np.sum()/())
+	return EnergyError,ForceError
 
 def GetForceEnergies():
-	managers = ["Mol_DavidMD_ANI1_Sym_Direct_fc_sqdiff_BP_Direct_Grad_Linear_1","Mol_DavidMetaMD_ANI1_Sym_Direct_fc_sqdiff_BP_Direct_Grad_Linear_1", "Mol_DavidNM_ANI1_Sym_Direct_fc_sqdiff_BP_Direct_Grad_Linear_1","Mol_DavidRandom_ANI1_Sym_Direct_fc_sqdiff_BP_Direct_Grad_Linear_1"]
+	managers = ["Mol_DavidMD_ANI1_Sym_Direct_fc_sqdiff_BP_Direct_Grad_Linear_1","Mol_DavidMetaMD_ANI1_Sym_Direct_fc_sqdiff_BP_Direct_Grad_Linear_1"] #, "Mol_DavidNM_ANI1_Sym_Direct_fc_sqdiff_BP_Direct_Grad_Linear_1","Mol_DavidRandom_ANI1_Sym_Direct_fc_sqdiff_BP_Direct_Grad_Linear_1"]
 	results={}
 	for i in managers:
 		ens,grads = GetEnergyAndForceFromManager(i)
@@ -43,6 +52,19 @@ def GetForceEnergies():
 	print results
 
 def TestOptimization(MName_):
+	"""
+	Distorts a set of molecules from their equilibrium geometries,
+	then optimizes the distorted geometries using a trained network.
+
+	Args:
+
+		MName_: Trained network
+
+	Returns:
+
+		np.mean(rms_list): Average value of all of the RMS errors for all molecules
+	"""
+
 	a = MSet("sampling_mols")
 	a.ReadXYZ()
 	TreatedAtoms = a.AtomTypes()
@@ -57,7 +79,9 @@ def TestOptimization(MName_):
 		tmp_rms = mol.rms_inv(molp)
 		rms_list.append(tmp_rms)
 		print "RMS:", tmp_rms
-	return rms_list
-# GetEnergyAndForceFromManager("Mol_DavidMetaMD_ANI1_Sym_Direct_fc_sqdiff_BP_Direct_Grad_Linear_1")
+	return np.mean(rms_list)
+
+
+GetEnergyAndForceFromManager("Mol_DavidMetaMD_ANI1_Sym_Direct_fc_sqdiff_BP_Direct_Grad_Linear_1", "DavidMetaMD")
 # GetForceEnergies()
-print TestOptimization("Mol_DavidMetaMD_ANI1_Sym_Direct_fc_sqdiff_BP_Direct_Grad_Linear_1")
+# print TestOptimization("Mol_DavidRandom_ANI1_Sym_Direct_fc_sqdiff_BP_Direct_Grad_Linear_1")
