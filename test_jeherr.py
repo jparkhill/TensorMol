2b9bf6344904ee4224784053b6868b865619816a
jeherr <johnherr@gmail.com>
2017-08-22 00:45:57 -0400

    symmetry func testing

--- test_jeherr.py
+++ test_jeherr.py
@@ -311,6 +311,94 @@ def train_forces_GauSH_direct(set_ = "SmallMols"):
 	tset = TensorDataDirect(a,d)
 	manager=TFManage("",tset,True,"fc_sqdiff_GauSH_direct_all")
 
+def TestTFSym():
+	np.set_printoptions(threshold=100000)
+	a=MSet("SmallMols_rand")
+	a.Load()
+	maxnatoms = a.MaxNAtoms()
+	zlist = []
+	xyzlist = []
+	natom = np.zeros((2), dtype=np.int32)
+	for i, mol in enumerate(a.mols):
+		paddedxyz = np.zeros((maxnatoms,3), dtype=np.float64)
+		paddedxyz[:mol.atoms.shape[0]] = mol.coords
+		paddedz = np.zeros((maxnatoms), dtype=np.int64)
+		paddedz[:mol.atoms.shape[0]] = mol.atoms
+		xyzlist.append(paddedxyz)
+		zlist.append(paddedz)
+		natom[i] = mol.NAtoms()
+		if i == 1:
+			break
+	xyzstack = tf.stack(xyzlist)
+	zstack = tf.stack(zlist)
+	NL = NeighborListSet(paddedxyz, natom, True, True, paddedz)
+	rad_p, ang_t = NL.buildPairsAndTriples(self.Rr_cut, self.Ra_cut)
+	Radp_pl=tf.Variable(rad_p, dtype=tf.int64,name="RadialPairs")
+	Angt_pl=tf.placeholder(ang_t, dtype=tf.int64,name="AngularTriples")
+
+	eles = [1,6,7,8]
+	n_eles = len(eles)
+	eles_np = np.asarray(eles).reshape((n_eles,1))
+	eles_pairs = []
+	for i in range (len(eles)):
+		for j in range(i, len(eles)):
+			eles_pairs.append([eles[i], eles[j]])
+	eles_pairs_np = np.asarray(eles_pairs)
+	Ele = tf.Variable(eles_np, trainable=False, dtype = tf.int64)
+	Elep = tf.Variable(eles_pairs_np, trainable=False, dtype = tf.int64)
+
+	Ra_cut = PARAMS["AN1_a_Rc"]
+	Rr_cut = PARAMS["AN1_r_Rc"]
+	zetas = np.array([[PARAMS["AN1_zeta"]]], dtype = prec)
+	etas = np.array([[PARAMS["AN1_eta"]]], dtype = prec)
+	AN1_num_a_As = PARAMS["AN1_num_a_As"]
+	AN1_num_a_Rs = PARAMS["AN1_num_a_Rs"]
+	thetas = np.array([ 2.0*Pi*i/AN1_num_a_As for i in range (0, AN1_num_a_As)], dtype = prec)
+	rs =  np.array([ Ra_cut*i/AN1_num_a_Rs for i in range (0, AN1_num_a_Rs)], dtype = prec)
+	# Create a parameter tensor. 4 x nzeta X neta X ntheta X nr
+	p1 = np.tile(np.reshape(zetas,[1,1,1,1,1]),[1,1,AN1_num_a_As,AN1_num_a_Rs,1])
+	p2 = np.tile(np.reshape(etas,[1,1,1,1,1]),[1,1,AN1_num_a_As,AN1_num_a_Rs,1])
+	p3 = np.tile(np.reshape(thetas,[1,1,AN1_num_a_As,1,1]),[1,1,1,AN1_num_a_Rs,1])
+	p4 = np.tile(np.reshape(rs,[1,1,1,AN1_num_a_Rs,1]),[1,1,AN1_num_a_As,1,1])
+	SFPa = np.concatenate([p1,p2,p3,p4],axis=4)
+	SFPa = np.transpose(SFPa, [4,0,1,2,3])
+	etas_R = np.array([[PARAMS["AN1_eta"]]], dtype = prec)
+	AN1_num_r_Rs = PARAMS["AN1_num_r_Rs"]
+	rs_R =  np.array([ Rr_cut*i/AN1_num_r_Rs for i in range (0, AN1_num_r_Rs)], dtype = prec)
+	# Create a parameter tensor. 2 x  neta X nr
+	p1_R = np.tile(np.reshape(etas_R,[1,1,1]),[1,AN1_num_r_Rs,1])
+	p2_R = np.tile(np.reshape(rs_R,[1,AN1_num_r_Rs,1]),[1,1,1])
+	SFPr = np.concatenate([p1_R,p2_R],axis=2)
+	SFPr = np.transpose(SFPr, [2,0,1])
+	# self.inshape = int(len(self.eles)*AN1_num_r_Rs + len(self.eles_pairs)*AN1_num_a_Rs*AN1_num_a_As)
+	# self.inshape_withencode = int(self.inshape + AN1_num_r_Rs)
+	#self.inshape = int(len(self.eles)*AN1_num_r_Rs)
+	p1 = np.tile(np.reshape(thetas,[AN1_num_a_As,1,1]),[1,AN1_num_a_Rs,1])
+	p2 = np.tile(np.reshape(rs,[1,AN1_num_a_Rs,1]),[AN1_num_a_As,1,1])
+	# SFPa2 = np.concatenate([p1,p2],axis=2)
+	# self.SFPa2 = np.transpose(SFPa2, [2,0,1])
+	# p1_new = np.reshape(rs_R,[AN1_num_r_Rs,1])
+	# self.SFPr2 = np.transpose(p1_new, [1,0])
+	zeta = PARAMS["AN1_zeta"]
+	eta = PARAMS["AN1_eta"]
+	# self.HasANI1PARAMS = True
+
+	SFPa2 = tf.Variable(np.transpose(np.concatenate([p1,p2],axis=2), [2,0,1]), trainable= False, dtype = tf.float64)
+	SFPr2 = tf.Variable(np.transpose(np.reshape(rs_R,[AN1_num_r_Rs,1]), [1,0]), trainable= False, dtype = tf.float64)
+	Rr_cut = tf.Variable(PARAMS["AN1_r_Rc"], trainable=False, dtype = tf.float64)
+	Ra_cut = tf.Variable(PARAMS["AN1_a_Rc"], trainable=False, dtype = tf.float64)
+	zeta = tf.Variable(PARAMS["AN1_zeta"], trainable=False, dtype = tf.float64)
+	eta = tf.Variable(PARAMS["AN1_eta"], trainable=False, dtype = tf.float64)
+	Scatter_Sym, Sym_Index  = TFSymSet_Scattered_Linear(xyzstack, zstack, Ele, SFPr2, Rr_cut, Elep, SFPa2, zeta, eta, Ra_cut, Radp_pl, Angt_pl)
+
+	# tmp = TF_gaussian_spherical_harmonics(xyzstack, zstack, labelstack, [1,6,7,8], gaussian_params, atomic_embed_factors, 4)
+	sess = tf.Session()
+	sess.run(tf.global_variables_initializer())
+	# for i in range(a.mols[0].atoms.shape[0]):
+	# 	print a.mols[0].atoms[i], "   ", a.mols[0].coords[i,0], "   ", a.mols[0].coords[i,1], "   ", a.mols[0].coords[i,2]
+	tmp2, tmp3 = sess.run(Scatter_Sym, Sym_Index)
+	print tmp2, tmp3
+
 
 # InterpoleGeometries()
 # ReadSmallMols(set_="SmallMols", forces=True, energy=True)
@@ -333,4 +421,5 @@ def train_forces_GauSH_direct(set_ = "SmallMols"):
 # TestTFBond()
 # GetPairPotential()
 # TestTFGauSH()
-train_forces_GauSH_direct("benzene_1rot")
+# train_forces_GauSH_direct("benzene_1rot")
+TestTFSym()
