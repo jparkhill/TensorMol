rotation constraint fix

TensorMol/RawEmbeddings.py
@@ -2690,8 +2690,8 @@ def TF_random_rotate(xyzs, rotation_params, labels = None, return_matrix = False
 		new_xyzs (tf.float): NMol x MaxNAtoms x 3 coordinates tensor of randomly rotated molecules
 		new_labels (tf.float): NMol x MaxNAtoms x label shape tensor of randomly rotated learning targets
 	"""
-	r = tf.sqrt(rotation_params[:,2]+1.0e-26)
-	v = tf.stack([tf.sin(rotation_params[:,1]) * r, tf.cos(rotation_params[:,1]) * r, tf.sqrt(2.0 - rotation_params[:,2]+1.0e-26)], axis=-1)
+	r = tf.sqrt(rotation_params[:,2])
+	v = tf.stack([tf.sin(rotation_params[:,1]) * r, tf.cos(rotation_params[:,1]) * r, tf.sqrt(2.0 - rotation_params[:,2])], axis=-1)
 	zero_tensor = tf.zeros_like(rotation_params[:,1])
 	R1 = tf.stack([tf.cos(rotation_params[:,0]), tf.sin(rotation_params[:,0]), zero_tensor], axis=-1)
 	R2 = tf.stack([-tf.sin(rotation_params[:,0]), tf.cos(rotation_params[:,0]), zero_tensor], axis=-1)

test_jeherr.py
@@ -483,7 +483,7 @@ def train_forces_rotation_constraint(set_ = "SmallMols"):
 	d = Digester(TreatedAtoms, name_="GauSH", OType_="Force")
 	tset = TensorDataDirect(a,d)
 	manager=TFManage("",tset,False,"fc_sqdiff_GauSH_direct_constrain_rotation")
-	manager.TrainElement(6)
+	manager.TrainElement(1)
 
 def test_tf_neighbor():
 	np.set_printoptions(threshold=100000)
@@ -546,7 +546,7 @@ def train_energy_pairs_triples():
 	PARAMS["NeuronType"] = "relu"
 	# PARAMS["tf_prec"] = "tf.float64"
 	# PARAMS["self.profiling"] = True
-	a=MSet("SmallMols_rand")
+	a=MSet("SmallMols")
 	a.Load()
 	TreatedAtoms = a.AtomTypes()
 	print "Number of Mols: ", len(a.mols)
@@ -579,10 +579,10 @@ def train_energy_pairs_triples():
 # TestTFSym()
 # train_energy_symm_func_channel()
 # test_gaussian_overlap()
-# train_forces_rotation_constraint("SmallMols")
+train_forces_rotation_constraint("SmallMols")
 # read_unpacked_set()
 # test_tf_neighbor()
-train_energy_pairs_triples()
+# train_energy_pairs_triples()
 
 # a=MSet("chemspider_aimd_forcecut")
 # a.Load()