4c9fb29a55a55819833714a5f2ccbf934c7cb1b3
Ryker McIntyre <rykermcintyre@rykers-mbp.dhcp.nd.edu>
2017-10-04 17:00:32 -0400

    Test nicotine ready for 5000 steps

--- test_ryker.py
+++ test_ryker.py
@@ -1,12 +1,12 @@
 from TensorMol import *
-import time
-import numpy as np
 import os
+import numpy as np
+import time
 
-def get_losses():
-	# Returns train_loss, energy_loss, and grad_loss vectors
-	# indexed properly for each step
-	with open("nicotine_aimd_log.txt","r") as log:
+def get_losses(filename):
+	# Returns train_loss, energy_loss, grad_loss, ...
+	# test_train_loss, test_energy_loss, test_grad_loss
+	with open(filename,"r") as log:
 		log = log.readlines()
 
 	keep_phrase = "TensorMol - INFO - step:"
@@ -14,24 +14,32 @@ def get_losses():
 	energy_loss = []
 	grad_loss = []
 
-	i = 0
+	test_train_loss = []
+	test_energy_loss = []
+	test_grad_loss = []
 
 	for line in log:
-		if keep_phrase in line:
+		if (keep_phrase in line) and (line[79] == ' '):
 			a = line.split()
 			train_loss.append(float(a[13]))
 			energy_loss.append(float(a[15]))
 			grad_loss.append(float(a[17]))
-			i += 1
+		if (keep_phrase in line) and (line[79] == 't'):
+			a = line.split()
+			test_train_loss.append(float(a[13]))
+			test_energy_loss.append(float(a[15]))
+			test_grad_loss.append(float(a[17]))
 
-	print(str(train_loss) + "\n\n" + str(energy_loss) + "\n\n" + str(grad_loss))
-	return train_loss, energy_loss, grad_loss
+	print(str(train_loss) + "\n\n" + str(energy_loss) + "\n\n" + str(grad_loss) + "\n")
+	print(str(test_train_loss) + "\n\n" + str(test_energy_loss) + "\n\n" + str(test_grad_loss) + "\n")
+	return train_loss, energy_loss, grad_loss, test_train_loss, test_energy_loss, test_grad_loss
 
 def optimize_taxol():
-	Taxol = MSet("Taxol").ReadXYZ("Taxol")
+	Taxol = MSet("Taxol")
+	Taxol.ReadXYZ()
 	GeomOptimizer("EnergyForceField").Opt(Taxol, filename="OptLog", Debug=False)
 
 
 
-get_losses()
+get_losses("networks/nicotine_aimd_log.txt")
 #optimize_taxol()
