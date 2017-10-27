c56e8b36a15ad59a0ccf9e541a8d9f4dcb0c4acd
Kun Yao <kyao@nd.edu>
2017-10-27 13:12:20 -0400

    trying to fix IR inten

--- test_chemspider12.py
+++ test_chemspider12.py
@@ -37,7 +37,7 @@ def TrainPrepare():
 		print(a.mols[100].properties)
                 a.Save()
 
-	if (1):
+	if (0):
                 a = MSet("chemspider12_clean")
 		a.Load()
 		b = MSet("chemspider12_clean_maxatom35")
@@ -47,7 +47,7 @@ def TrainPrepare():
 				b.mols.append(mol)
                 b.Save()
 
-	if (1):
+	if (0):
                 a = MSet("chemspider12_clean_maxatom35")
 		a.Load()
 		random.shuffle(a.mols)
@@ -62,9 +62,41 @@ def TrainPrepare():
 			c.mols.append(a.mols[i])
                	c.Save()
 		
-
-	
-
+#H2O_wbxd_1to21_with_prontonated_with_ch4.dat
+	if (1):
+		WB97XDAtom={}
+		WB97XDAtom[1]=-0.5026682866
+		WB97XDAtom[6]=-37.8387398698
+		WB97XDAtom[7]=-54.5806161811
+		WB97XDAtom[8]=-75.0586028656
+                a = MSet("chemspider12_maxatom35_H2O_with_CH4")
+                dic_list = pickle.load(open("./datasets/H2O_wbxd_1to21_with_prontonated_with_ch4.dat", "rb"))
+                for mol_index, dic in enumerate(dic_list):
+                        atoms = []
+			print ("mol_index:", mol_index)
+                        for atom in dic['atoms']:
+                                atoms.append(AtomicNumber(atom))
+                        atoms = np.asarray(atoms, dtype=np.uint8)
+                        mol = Mol(atoms, dic['xyz'])
+			if mol.NAtoms() <= 35:
+                        	mol.properties['dipole'] = np.asarray(dic['dipole'])
+                        	mol.properties['energy'] = dic['scf_energy']
+                        	mol.properties['gradients'] = dic['gradients']
+				mol.properties['atomization'] = dic['scf_energy']
+				for i in range (0, mol.NAtoms()):
+					mol.properties['atomization'] -= WB97XDAtom[mol.atoms[i]]
+                        	a.mols.append(mol)
+		
+		
+                b = MSet("chemspider12_clean_maxatom35")
+		b.Load()
+		print ("nmol in water/ch4:", len(a.mols))
+		print ("nmol in chemspider12:", len(b.mols))
+		random.shuffle(b.mols)
+		for i in range(0, len(b.mols)-len(a.mols)):
+			a.mols.append(b.mols[i])
+		print ("nmol in  water/ch4+chemspider12:", len(b.mols))
+		a.Save()
 
 def Train():
 	if (0):
@@ -215,8 +247,48 @@ def Train():
 		PARAMS['Profiling']=0
 		manager.Train(1)
 
-	if (1): # Normalize+Dropout+500+usual, dropout07+sigmoid100+rightalpha
+	if (0): # Normalize+Dropout+500+usual, dropout07+sigmoid100+rightalpha
 		a = MSet("chemspider12_clean_maxatom35")
+		a.Load()
+		random.shuffle(a.mols)
+		TreatedAtoms = a.AtomTypes()
+		PARAMS["NetNameSuffix"] = "act_sigmoid100_rightalpha"
+		PARAMS["learning_rate"] = 0.00001
+		PARAMS["momentum"] = 0.95
+		PARAMS["max_steps"] = 21
+		PARAMS["batch_size"] =  50   # 40 the max min-batch size it can go without memory error for training
+		PARAMS["test_freq"] = 1
+		PARAMS["tf_prec"] = "tf.float64"
+		PARAMS["EnergyScalar"] = 1.0
+		PARAMS["GradScalar"] = 1.0/20.0
+		PARAMS["DipoleScaler"]=1.0
+		PARAMS["NeuronType"] = "sigmoid_with_param"
+		PARAMS["sigmoid_alpha"] = 100.0
+		PARAMS["HiddenLayers"] = [2000, 2000, 2000]
+		PARAMS["EECutoff"] = 15.0
+		PARAMS["EECutoffOn"] = 0
+		#PARAMS["Erf_Width"] = 1.0
+		#PARAMS["Poly_Width"] = 4.6
+		PARAMS["Elu_Width"] = 4.6  # when elu is used EECutoffOn should always equal to 0
+		#PARAMS["AN1_r_Rc"] = 8.0
+		#PARAMS["AN1_num_r_Rs"] = 64
+		PARAMS["EECutoffOff"] = 15.0
+		PARAMS["DSFAlpha"] = 0.18
+		PARAMS["AddEcc"] = True
+		PARAMS["KeepProb"] = [1.0, 1.0, 1.0, 0.7]
+		#PARAMS["KeepProb"] = 0.7
+		PARAMS["learning_rate_dipole"] = 0.0001
+		PARAMS["learning_rate_energy"] = 0.00001
+		PARAMS["SwitchEpoch"] = 2
+		d = MolDigester(TreatedAtoms, name_="ANI1_Sym_Direct", OType_="EnergyAndDipole")  # Initialize a digester that apply descriptor for the fragme
+		tset = TensorMolData_BP_Direct_EE_WithEle(a, d, order_=1, num_indis_=1, type_="mol",  WithGrad_ = True)
+		manager=TFMolManage("",tset,False,"fc_sqdiff_BP_Direct_EE_ChargeEncode_Update_vdw_DSF_elu_Normalize_Dropout")
+		PARAMS['Profiling']=0
+		manager.Train(1)
+#chemspider12_maxatom35_H2O_with_CH4
+
+	if (1): # Normalize+Dropout+500+usual, dropout07+sigmoid100+rightalpha
+		a = MSet("chemspider12_maxatom35_H2O_with_CH4")
 		a.Load()
 		random.shuffle(a.mols)
 		TreatedAtoms = a.AtomTypes()
@@ -331,6 +403,9 @@ def Eval():
 	if (1):
 		a=MSet("aspirin", center_=False)
 		a.ReadXYZ("aspirin")
+		#chemspider_IR_test_mol
+		#a=MSet("chemspider_IR_test_mol", center_=False)
+		#a.ReadXYZ("chemspider_IR_test_mol")
 		TreatedAtoms = np.array([1,6,7,8], dtype=np.uint8)
 		PARAMS["NetNameSuffix"] = "act_sigmoid100"
 		PARAMS["learning_rate"] = 0.00001
@@ -365,7 +440,8 @@ def Eval():
 		tset = TensorMolData_BP_Direct_EE_WithEle(a, d, order_=1, num_indis_=1, type_="mol",  WithGrad_ = True)
 		manager=TFMolManage("Mol_chemspider12_clean_maxatom35_ANI1_Sym_Direct_fc_sqdiff_BP_Direct_EE_ChargeEncode_Update_vdw_DSF_elu_Normalize_Dropout_act_sigmoid100", tset,False,"fc_sqdiff_BP_Direct_EE_ChargeEncode_Update_vdw_DSF_elu_Normalize_Dropout",False,False)
 
-		m = a.mols[-1]
+		m = a.mols[1]
+		eq_coords = m.coords.copy()
 		#print manager.EvalBPDirectEEUpdateSinglePeriodic(m, PARAMS["AN1_r_Rc"], PARAMS["AN1_a_Rc"], PARAMS["EECutoffOff"], m.NAtoms())
 		#print manager.EvalBPDirectEEUpdateSingle(m, PARAMS["AN1_r_Rc"], PARAMS["AN1_a_Rc"], PARAMS["EECutoffOff"], True)
 		#return
@@ -397,16 +473,22 @@ def Eval():
 
 		def EnergyField(x_):
 			return EnAndForce(x_,False)[0]
+
+		def DipoleField(x_):
+			#q = ChargeField(x_)
+			q = ChargeField(eq_coords)
+			dipole = Dipole(x_, q)
+			return dipole
+
 		#ForceField = lambda x: EnAndForce(x)[-1]
 		#EnergyField = lambda x: EnAndForce(x)[0]
 		EnergyForceField = lambda x: EnAndForce(x)
 
-		PARAMS["OptMaxCycles"]=1
+		PARAMS["OptMaxCycles"]=200
 		Opt = GeomOptimizer(EnAndForce)
 		m=Opt.Opt(m)
-		return
  		masses = np.array(map(lambda x: ATOMICMASSESAMU[x-1],m.atoms))
-		w,v = HarmonicSpectra(EnergyField, m.coords, m.atoms)
+		w,v = HarmonicSpectra(EnergyField, m.coords, m.atoms, WriteNM_=True, Mu_ = DipoleField)
 		return
 		##m.coords[0] = m.coords[0] + 0.1
                 #PARAMS["MDThermostat"] = "Nose"
