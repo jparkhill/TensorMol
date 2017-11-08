64beb2ed97b1a338bc42715d3d8f6ae425b5fffe
John Parkhill <john.parkhill@gmail.com>
2017-11-08 13:42:25 -0500

    autodoc

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,7 @@ print sys.path
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc','sphinx.ext.todo',
-	'sphinxcontrib.napoleon',
+	'sphinx.ext.napoleon',
     'sphinx.ext.imgmath']
 
 # Add any paths that contain templates here, relative to this directory.
