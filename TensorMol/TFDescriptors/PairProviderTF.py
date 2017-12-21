387acd41ffc542ee9720237b9b1c134b14793578
John Parkhill <jparkhil@nd.edu>
2017-12-21 11:04:00 -0500

    Update PairProviderTF.py

--- TensorMol/TFDescriptors/PairProviderTF.py
+++ TensorMol/TFDescriptors/PairProviderTF.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from TensorMol.ElectrostaticsTF import *
+from ..ForceModels.ElectrostaticsTF import *
 from .RawEmbeddings import *
 
 class PairProvider:
