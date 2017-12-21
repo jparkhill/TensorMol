aba362b0907d983eef2c79d67e4f4ad22a7acc02
John Parkhill <john.parkhill@gmail.com>
2017-12-20 23:25:40 -0500

    Training and evaluation working

--- TensorMol/TFNetworks/TFInstance.py
+++ TensorMol/TFNetworks/TFInstance.py
@@ -7,6 +7,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from ..Containers.TensorData import *
+from ..ForceModifiers.Transformer import *
 from ..TFDescriptors.RawEmbeddings import *
 from ..Util import *
 import numpy as np
